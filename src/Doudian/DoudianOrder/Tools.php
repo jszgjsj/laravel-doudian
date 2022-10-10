b2790d4a972793791f9a30a10d959b53840c601e
sangyu <sangyu@runsnailrun.com>
2022-10-10 16:42:54 +0800

    这是我的第一次提交“hello word”

---
+++ src/Doudian/DoudianOrder/Tools.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Doudian\DoudianOrder;
+
+
+class Tools
+{
+    public  function __construct()
+    {
+    }
+
+    public static function Hello()
+    {
+        echo "Hello Doudian";
+    }
+}
